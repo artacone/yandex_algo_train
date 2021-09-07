7dd90b5081d0bd32d9232e9e68b117414639b099
artacone <artacone@gmail.com>
2021-09-07 21:18:48 +0700

    Fix

--- 2/G/G.go
+++ 2/G/G.go
@@ -5,30 +5,31 @@ import (
 )
 
 func main() {
-	min, secondToMin := 1, 1
-	max, secondToMax := -1, -1
-	var n int
+	min, secondToMin := 1000000, 1000000
+	max, secondToMax := -1000000, -1000000
+	var n, count int
 	for {
 		scan, _ := fmt.Scanf("%d", &n)
 		if scan == 0 {
 			break
 		}
-		if n < 0 {
-			if n < min {
-				secondToMin, min = min, n
-			} else if n < secondToMin {
-				secondToMin = n
-			}
-		} else {
-			if n > max {
-				secondToMax, max = max, n
-			} else if n > secondToMax {
-				secondToMax = n
-			}
+		if n < min {
+			secondToMin, min = min, n
+		} else if n < secondToMin {
+			secondToMin = n
 		}
+		if n > max {
+			secondToMax, max = max, n
+		} else if n > secondToMax {
+			secondToMax = n
+		}
+		count++
 	}
-	maxProduct, minProduct := max*secondToMax, min*secondToMin
-	if maxProduct > minProduct {
+	if count == 2 {
+		fmt.Println(min, max)
+		return
+	}
+	if max*secondToMax > min*secondToMin {
 		fmt.Println(secondToMax, max)
 	} else {
 		fmt.Println(min, secondToMin)
