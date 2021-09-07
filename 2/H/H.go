46f3116e41a3cf16fa79f64181ca43175827ffc4
artacone <artacone@gmail.com>
2021-09-07 22:16:53 +0700

    Add 2H (CE)

---
+++ 2/H/H.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	max := [3]int{-1000000, -1000000, -1000000}
+	min := [2]int{1000000, 1000000}
+	var n int
+	for {
+		scan, _ := fmt.Scanf("%d", &n)
+		if scan == 0 {
+			break
+		}
+		if n > max[2] {
+			max[0], max[1], max[2] = max[1], max[2], n
+		} else if n > max[1] {
+			max[0], max[1] = max[1], n
+		} else if n > max[0] {
+			max[0] = n
+		}
+		if n < min[0] {
+			min[1], min[0] = n, min[1]
+		} else if n < min[1] {
+			min[1] = n
+		}
+	}
+	if min[0]*min[1]*max[2] > max[0]*max[1]*max[2] {
+		fmt.Println(min[0], min[1], max[2])
+	} else {
+		fmt.Println(max[0], max[1], max[2])
+	}
+}
